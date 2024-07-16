03400d304ce1fdc19719d11ccf910b710d8f2901
Darya Litvinova <lit.darya876@gmail.com>
2024-07-16 11:19:34 +0200

    change logic of prime-game

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const isPrime = (randomNumber) => {
   if (randomNumber < 2) {
     return false;
   }
-  for (let i = 2; i < randomNumber / 2; i += 1) {
+  for (let i = 2; i < randomNumber; i += 1) {
     if (randomNumber % i === 0) {
       return false;
     }
